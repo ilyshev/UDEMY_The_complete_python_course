01d21f147dd8eb554c8139e448ac71425837260d
ilyshev <dilyshev@mac.com>
2020-05-27 12:59:20 -0400

    Creating database in file

--- Milestone_2_lists/app.py
+++ Milestone_2_lists/app.py
@@ -8,10 +8,11 @@ Enter:
 - 'd' to delete a book
 - 'q' to quit
 
-Your choice:
-'''
+Your choice: '''
+
 
 def menu():
+    database.create_book_table()
     user_input = input(USER_CHOICE)
     while user_input != 'q':
         if user_input == 'a':
@@ -25,6 +26,8 @@ def menu():
         else:
             print("Unknown command. Please try again.")
 
+        user_input = input((USER_CHOICE))
+
 
 def prompt_add_book():
     name = input('Enter the book name: ')
@@ -36,7 +39,7 @@ def prompt_add_book():
 def list_books():
     books = database.get_all_books()
     for book in books:
-        read = "YES" if book['read'] else 'NO'
+        read = "YES" if book['read'] == '1' else 'NO'
         print(f"{book['name']} by {book['author']}, read: {read}")
 
 
