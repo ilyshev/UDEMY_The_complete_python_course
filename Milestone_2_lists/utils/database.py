9fe757c710ee22db7fdeba4f984000e76c717f0e
ilyshev <dilyshev@mac.com>
2020-05-27 18:29:36 -0400

    added types and hinting

--- Milestone_2_lists/utils/database.py
+++ Milestone_2_lists/utils/database.py
@@ -1,23 +1,27 @@
+from typing import List, Dict, Union
+
 from .database_connection import DatabaseConnection
 
 '''
 Concerned with storing and retrieving books from a database.
 '''
 
+Book = Dict[str, Union[str, int]]
 
-def create_book_table():
+
+def create_book_table() -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('CREATE TABLE books(name text, author text, read integer)')
 
 
-def add_book(name, author):
+def add_book(name: str, author: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('INSERT INTO books VALUES(?,?,0)', (name, author))
 
 
-def get_all_books():
+def get_all_books() -> List[Book]:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM books')
@@ -27,13 +31,13 @@ def get_all_books():
     return books
 
 
-def mark_book_as_read(name):
+def mark_book_as_read(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
 
-def delete_book(name):
+def delete_book(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('DELETE FROM books WHERE name=?', (name,))
